d4b84309b3b6564640f29cf12617e2369ce3863d
Sagar Dighe <sagasoftech@gmail.com>
2023-04-10 23:44:46 -0500

    Private constructor example

---
+++ src/com/corejava/constructor/PrivateConstructor.java
@@ -0,0 +1,18 @@
+package com.corejava.constructor;
+
+/*
+ * Scenarios: 
+ * 1. Internal Constructor chaining
+ * 2. Singleton class design pattern
+ */
+public class PrivateConstructor {
+
+    
+    /*
+     * Private constructor can't be accessed outside the class
+     */
+    private PrivateConstructor() {  
+    	
+    }
+    
+}
