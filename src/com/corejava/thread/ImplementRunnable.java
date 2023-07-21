4cdbd3174fd6f8027c3b705fbcb570b03b9a91fc
Sagar Dighe <sagasoftech@gmail.com>
2023-07-21 00:31:28 -0500

    Multithreading using Implement Runnable

---
+++ src/com/corejava/thread/ImplementRunnable.java
@@ -0,0 +1,23 @@
+package com.corejava.thread;
+
+public class ImplementRunnable implements Runnable{
+
+	private int threadName = 0;
+	
+	ImplementRunnable(int threadName){
+		this.threadName = threadName;
+	}
+	
+	@Override
+	public void run() {
+		for(int i = 0; i < 5; i++) {
+			System.out.println(i + " from thread " + threadName);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- src/com/corejava/thread/MainThread.java
+++ src/com/corejava/thread/MainThread.java
@@ -3,9 +3,15 @@ package com.corejava.thread;
 public class MainThread {
 
 	public static void main(String[] args) {
-		
+		/*
 		for(int i = 0; i <=3; i++) {
 			ExtendThread thread = new ExtendThread(i);
+			thread.start();
+		}
+		*/
+		for(int i = 0; i <=3; i++) {
+			ImplementRunnable runnable = new ImplementRunnable(i);
+			Thread thread = new Thread(runnable);
 			thread.start();
 		}
 	}
