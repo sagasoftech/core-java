f7dfd1e77ecff0ba4e9f6c2f50a575f479806272
Sagar Dighe <sagasoftech@gmail.com>
2023-07-30 17:55:20 -0500

    Remove element and shift array

---
+++ src/com/corejava/interviewprograms/RemoveElementAndShiftArray.java
@@ -0,0 +1,50 @@
+package com.corejava.interviewprograms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+ * The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ * 
+ * Consider the number of elements in nums which are not equal to val be k, to get accepted, 
+ * you need to do the following things:
+ * 
+ * Change the array nums such that the first k elements of nums contain the elements 
+ * which are not equal to val. The remaining elements of nums are not important as well as the size of nums
+ * 
+ * Return k.
+
+ */
+public class RemoveElementAndShiftArray {
+
+	public static void main(String[] args) {
+		
+		int[] nums = new int[] {0,1,2,2,3,0,4,2};
+		int val = 2;
+		
+		//Result: Array: [0, 1, 3, 0, 4, -1, -1, -1]
+		System.out.println("Length after remove: " + removeElement(nums, val));
+		System.out.println("Array: " + Arrays.toString(nums));
+	}
+	
+    public static int removeElement(int[] nums, int val) {
+        int counter = 0;
+        List<Integer> placeToFill = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == val){
+            	placeToFill.add(i);
+                
+                nums[i] = -1;
+                counter++;
+            }else if(placeToFill.size() > 0){
+                nums[placeToFill.get(0)] = nums[i];
+                placeToFill.remove(0);
+                nums[i] = -1;
+                placeToFill.add(i);
+            }
+        }
+        return nums.length - counter;
+    }
+}
