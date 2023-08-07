aa859295a0a288b0787e5b858f76ed6a72c28ced
Sagar Dighe <sagasoftech@gmail.com>
2023-08-06 23:40:36 -0500

    Linear solution

--- src/com/corejava/dynamicprograming/CanSumInArray.java
+++ src/com/corejava/dynamicprograming/CanSumInArray.java
@@ -1,5 +1,8 @@
 package com.corejava.dynamicprograming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * Write a function 'canSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments.
  * 
@@ -14,6 +17,7 @@ public class CanSumInArray {
 
 	public static void main(String[] args) {
 		System.out.println(canSum(7 , new int[]{5, 3, 4, 7}));
+		System.out.println(canSumLinear(800 , new int[]{5, 3, 4, 7}));
 	}
 	
 	/*
@@ -28,6 +32,27 @@ public class CanSumInArray {
 		for(int num = 0; num < numbers.length; num++) {
 			int remainder = targetSum - numbers[num];
 			if(canSum(remainder, numbers)) {
+				return true;
+			}
+		}
+	
+		return false;
+	}
+	
+	/*
+	 * Time Complexity: O(n*m)
+	 * Space Complexity: O(m)
+	 */
+	private static Map<Integer, Boolean> hmTargetSum = new HashMap<>();
+	public static boolean canSumLinear(int targetSum, int[] numbers) {
+		if(hmTargetSum.containsKey(targetSum)) return hmTargetSum.get(targetSum);
+		if(targetSum == 0) return true;
+		if(targetSum < 0) return false;
+		
+		for(int num = 0; num < numbers.length; num++) {
+			int remainder = targetSum - numbers[num];
+			if(canSumLinear(remainder, numbers)) {
+				hmTargetSum.put(remainder, true);
 				return true;
 			}
 		}
