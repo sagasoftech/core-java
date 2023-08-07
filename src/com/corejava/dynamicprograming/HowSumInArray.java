c9bd1ee4641181e9359b41c71cf7c5936c6dcbe8
Sagar Dighe <sagasoftech@gmail.com>
2023-08-07 01:45:46 -0500

    Linear complexity using memorization

--- src/com/corejava/dynamicprograming/HowSumInArray.java
+++ src/com/corejava/dynamicprograming/HowSumInArray.java
@@ -1,7 +1,9 @@
 package com.corejava.dynamicprograming;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * Write a function 'howSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments.
@@ -25,6 +27,15 @@ public class HowSumInArray {
 		System.out.println(howSum(8 , new int[]{2, 3, 4}));
 		System.out.println(howSum(7 , new int[]{2, 4}));
 		System.out.println(howSum(0 , new int[]{1, 3, 2}));
+		
+		System.out.println(howSumMemoUse(7 , new int[]{5, 3, 4, 7}));
+		System.out.println(howSumMemoUse(8 , new int[]{2, 3, 4}));
+		System.out.println(howSumMemoUse(7 , new int[]{2, 4}));
+		System.out.println(howSumMemoUse(0 , new int[]{1, 3, 2}));
+		
+		System.out.println(howSumMemoUse(300 , new int[]{7, 14}));
+		
+		
 	}
 	
 	/*
@@ -46,5 +57,34 @@ public class HowSumInArray {
 		}
 	
 		return null;
+	}
+	
+	/*
+	 * Time Complexity: O(n*m^2)
+	 * Space Complexity: O(m)
+	 */
+	public static List<Integer> howSumMemoUse(int targetSum, int[] numbers, Map<Integer, List<Integer>> hmTargetArr) {
+		
+		if(hmTargetArr.containsKey(targetSum)) return hmTargetArr.get(targetSum);
+		
+		if(targetSum == 0) return new ArrayList<Integer>();
+		if(targetSum < 0) return null;
+		
+		for(int num = 0; num < numbers.length; num++) {
+			int remainder = targetSum - numbers[num];
+			List<Integer> remainderResult = howSumMemoUse(remainder, numbers, hmTargetArr);
+			if(remainderResult != null) {
+				remainderResult.add(numbers[num]);
+				hmTargetArr.put(targetSum, remainderResult);
+				return hmTargetArr.get(targetSum);
+			}
+		}
+	
+		hmTargetArr.put(targetSum, null);
+		return null;
+	}
+	
+	public static List<Integer> howSumMemoUse(int targetSum, int[] numbers) {
+		return howSumMemoUse(targetSum, numbers, new HashMap<>());
 	}
 }
