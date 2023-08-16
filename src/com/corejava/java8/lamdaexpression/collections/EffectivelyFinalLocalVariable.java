e8737c1ef71c4303b06ab3ea7517f14eb8f3fe6a
Sagar Dighe <sagasoftech@gmail.com>
2023-08-15 23:29:39 -0500

    Illustrating Difference between Effectively final and Mutable Local Variables:
    As the comments indicate, number is effectively final and can, therefore, be used inside myLambda. However, if number were to be modified, either inside the lambda or outside of it, number would lose its effective final status. This would cause an error, and the program would not compile.

---
+++ src/com/corejava/java8/lamdaexpression/collections/EffectivelyFinalLocalVariable.java
@@ -0,0 +1,39 @@
+package com.corejava.java8.lamdaexpression.collections;
+
+public class EffectivelyFinalLocalVariable {
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+  
+        // Custom local variable that can be captured
+        int number = 10;
+  
+        MyFunction myLambda = (n) ->
+        {
+  
+            // This use of number is OK It does not modify
+            // num
+            int value = number + n;
+  
+            // However, the following is illegal because it
+            // attempts to modify the value of number
+  
+            // number++;
+            return value;
+        };
+  
+        // The following line would also cause an error,
+        // because it would remove the effectively final
+        // status from num. 
+        //number = 9;
+  
+        System.out.println("EffectivelyFinalLocalVariable!");
+    }
+}
+
+interface MyFunction  {
+
+	 // Method inside the interface
+	int func(int n);
+}
